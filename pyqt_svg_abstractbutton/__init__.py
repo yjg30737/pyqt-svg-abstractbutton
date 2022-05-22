222cea65bf0b88b691763592da3a20681ff6d1ac
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-22 14:43:48 +0900

    First commit from local

---
+++ pyqt_svg_abstractbutton/__init__.py
@@ -0,0 +1,1 @@
+from .svgAbstractButton import SvgAbstractButton
---
+++ setup.py
@@ -0,0 +1,25 @@
+import codecs
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+setup(
+    name='pyqt-svg-abstractbutton',
+    version='0.0.1',
+    author='Jung Gyu Yoon',
+    author_email='yjg30737@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    description='PyQt QAbstractButton for pyqt-svg-button and pyqt-svg-toolbutton',
+    url='https://github.com/yjg30737/pyqt-svg-abstractbutton.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    install_requires=[
+        'PyQt5>=5.8'
+    ]
+)
